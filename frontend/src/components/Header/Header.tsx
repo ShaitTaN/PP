01fb1c1000e55bf23be212bbccb07364a7a5ad96
Dmitriy <kdima18@mail.ru>
2022-12-14 14:51:39 +0300

    burger menu

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,37 +1,50 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase";
 import "./header.css";
 
 interface HeaderProps {
-	isAuthorized: boolean;
-	setIsAuthorized: (isAuthorized: boolean) => void;
-	tgUser: any;
+  isAuthorized: boolean;
+  setIsAuthorized: (isAuthorized: boolean) => void;
+  tgUser: any;
 }
 
-const Header: React.FC<HeaderProps> = ({isAuthorized, setIsAuthorized, tgUser}) => {
+const Header: React.FC<HeaderProps> = ({isAuthorized,setIsAuthorized,tgUser,}) => {
+	const [isMenuActive, setIsMenuActive] = React.useState(false);
 
-	const onLogout = () => {
-		auth.signOut().then(()=>{
-			setIsAuthorized(false)
-		})
+  const onLogout = () => {
+    auth.signOut().then(() => {
+      setIsAuthorized(false);
+    });
+  };
+
+	const onToggleMenu = () => {
+		setIsMenuActive(!isMenuActive);
 	}
 
   return (
-    <header className="header">
-			
-      <Link className="header__link" to={"/"}>
-        Главная
-      </Link>
-      <Link className="header__link" to={"/serial"}>
-        Серийный номер
-      </Link>
-      {isAuthorized && tgUser && (
-        <div className="header__user">
-          <span>{tgUser?.username || "USERNAME"}</span>
-          <button onClick={onLogout}>Выход</button>
-        </div>
-      )}
-    </header>
+    <>
+      <div className={`burger ${isMenuActive ? 'active' : ''}`} onClick={onToggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <header className={`header ${isMenuActive ? 'active' : ''}`}>
+        <Link className="header__link" to={"/"}>
+          Главная
+        </Link>
+        <Link className="header__link" to={"/serial"}>
+          Серийный номер
+        </Link>
+        {/* {isAuthorized && tgUser && ( */}
+        {isAuthorized && (
+          <div className="header__user">
+            <span>{tgUser?.username || "USERNAME"}</span>
+            <button onClick={onLogout}>Выход</button>
+          </div>
+        )}
+      </header>
+    </>
   );
 };
 

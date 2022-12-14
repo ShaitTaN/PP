3de37050c62d6d85e5d15c88ee4b53d5f2e2c6fa
Dmitriy <kdima18@mail.ru>
2022-12-14 23:33:02 +0300

    fix

--- frontend/src/pages/SerialCodePage/SerialCodePage.tsx
+++ frontend/src/pages/SerialCodePage/SerialCodePage.tsx
@@ -20,54 +20,55 @@ const SerialCodePage: React.FC<SerialCodePageProps> = ({ userGroup }) => {
   const [serialCode, setSerialCode] = React.useState("");
   const [data, setData] = React.useState<SerialCode | null>(null);
   const [isActive, setIsActive] = React.useState(false);
-	const {queryId, tg, user: tgUser} = useTelegram()
+  const { tg, user: tgUser } = useTelegram();
 
   const onSendSerialCode = React.useCallback(async () => {
-		if(!serialCode) return;
+    if (!serialCode) return;
     try {
-      const res = await fetch("http://localhost:3030/serial", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ serialCode, queryId }),
-      });
-      const data = await res.json();
-			if(!tgUser){
-				setIsActive(true);
-				setData(data);
-			}
+      if (!tgUser) {
+        const res = await fetch("http://localhost:3030/serial", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ serialCode }),
+        });
+        const data = await res.json();
+
+        setIsActive(true);
+        setData(data);
+      }
+      tg.sendData(JSON.stringify({ serialCode, msg: "get_serial_code" }));
     } catch (error) {
       console.log(error);
     }
-  }, [serialCode, queryId, tgUser]);
+  }, [serialCode, tg, tgUser]);
 
-		// Подписка на событие нажатия на main телеграм кнопку
-		React.useEffect(() => {
-			tg.MainButton.onClick(onSendSerialCode);
-			return () => {
-				tg.MainButton.offClick(onSendSerialCode);
-			};
-		}, [tg, onSendSerialCode]);
-	
-		// Изменение текста main телеграм кнопки и ее отображение
-		React.useEffect(() => {
-			tg?.MainButton.setParams({ text: "Отправить" });
-			tg?.MainButton.show();
-			return () => {
-				tg?.MainButton.hide();
-			};
-		}, [tg.MainButton]);
-	
-		// Отключение main телеграм кнопки при незаполненных полях
-		React.useEffect(() => {
-			if (!serialCode) {
-				tg?.MainButton.disable();
-			} else {
-				tg?.MainButton.enable();
-			}
-		}, [tg.MainButton, serialCode]);
-	
+  // Подписка на событие нажатия на main телеграм кнопку
+  React.useEffect(() => {
+    tg.MainButton.onClick(onSendSerialCode);
+    return () => {
+      tg.MainButton.offClick(onSendSerialCode);
+    };
+  }, [tg, onSendSerialCode]);
+
+  // Изменение текста main телеграм кнопки и ее отображение
+  React.useEffect(() => {
+    tg?.MainButton.setParams({ text: "Отправить" });
+    tg?.MainButton.show();
+    return () => {
+      tg?.MainButton.hide();
+    };
+  }, [tg.MainButton]);
+
+  // Отключение main телеграм кнопки при незаполненных полях
+  React.useEffect(() => {
+    if (!serialCode) {
+      tg?.MainButton.disable();
+    } else {
+      tg?.MainButton.enable();
+    }
+  }, [tg.MainButton, serialCode]);
 
   return (
     <div className="serialCodePage">
@@ -78,7 +79,9 @@ const SerialCodePage: React.FC<SerialCodePageProps> = ({ userGroup }) => {
           value={serialCode}
           onChange={(e) => setSerialCode(e.target.value.trim())}
         />
-        {!queryId && <MainButton onClick={onSendSerialCode}>Отправить</MainButton>}
+        {!tgUser && (
+          <MainButton onClick={onSendSerialCode}>Отправить</MainButton>
+        )}
       </div>
       {!data && isActive && <p>Такого серийного номера не существует!</p>}
       {data && (
